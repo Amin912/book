7c48d5b8e6e660a25c70535a8d1c5000235d060c
mohammedaminezougagh <mohammedaminezougagh@gmail.com>
2020-12-26 02:00:38 +0100

    commited

---
+++ script.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+// 	$(".search-icon").click(function() {
+// 		$(this).css("width", "200px");
+// 	});
+
+
+// });
+
+function toggle()
+{
+	var x=document.getElementById("menu");
+	x.style.display==='none'? x.style.display='block':x.style.display='none';
+	
+}
+
+function showInput()
+{
+	var a= document.createElement("input");
+	this.insertBefore(a);
+}
+
